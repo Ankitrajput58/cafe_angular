3ba27ef4ccac1b970039330560af513876e07074
Ankitrajput58 <rajputankit5896@gmail.com>
2023-12-09 11:39:41 +0530

    angular_signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA,ApplicationConfig } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzButtonModule } from 'ng-zorro-antd/button'; 
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { SignupComponent } from './auth-components/signup/signup.component';
+
+
+import { RouterModule } from '@angular/router';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon'; // Make sure to include NzIconModule if you are using icons
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NzButtonModule,
+    NzSpinModule,
+    RouterModule,
+    NzFormModule,
+    NzInputModule,
+    NzIconModule,
+    NzNotificationModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
