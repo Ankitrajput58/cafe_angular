3256b6ade106aea654e1bb26f0ee2261c93fbfd0
Ankitrajput58 <rajputankit5896@gmail.com>
2023-12-10 16:53:26 +0530

    token,userRole store in local storage

--- src/app/auth-components/login/login.component.ts
+++ src/app/auth-components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../auth-services/auth-service/auth.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BooleanInput } from 'ng-zorro-antd/core/types';
+import { StorageService } from '../../auth-services/storage-service/storage.service';
 
 
 @Component({
@@ -28,6 +29,17 @@ export class LoginComponent {
   submitForm() {
     this.service.login(this.loginForm.value).subscribe((res)=>{
       console.log(res);
+      if(res.userId != null){
+        const user ={
+          id:res.userId,
+          role:res.userRole
+        }
+        console.log(user);
+        StorageService.saveToken(res.jwt);
+        StorageService.saveUser(user)
+      }else{
+        console.log("Wrong credentials !!")
+      }
     });
     
     }
